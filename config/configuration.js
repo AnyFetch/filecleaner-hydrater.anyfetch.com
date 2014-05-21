406f319e6b987c6bd8fd1dba5925334699b62b91
Amoki <hugoduroux@gmail.com>
2014-05-21 19:57:32 +0200

    remove a useless config

--- config/configuration.js
+++ config/configuration.js
@@ -7,9 +7,8 @@ var node_env = process.env.NODE_ENV || "development";
 var default_port = 8000;
 
 // Number of pdf instance to run simultaneously per cluster
-var default_concurrency = 4;
+var default_concurrency = 8;
 
-var default_api_url="https://api.anyfetch.com";
 if(node_env === "production") {
   default_port = 80;
 }
@@ -18,6 +17,5 @@ if(node_env === "production") {
 module.exports = {
   env: node_env,
   port: process.env.PORT || default_port,
-  api_url: process.env.API_URL || default_api_url,
   concurrency: process.env.HYDRATER_FILECLEANER_CONCURRENCY || default_concurrency
 };
