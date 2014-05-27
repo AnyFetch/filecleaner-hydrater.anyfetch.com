aedf94d3ecf2178f2d65ac519940a70fc4c87b14
Amoki <hugoduroux@gmail.com>
2014-05-27 13:37:32 +0200

    no more 72 changes

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ require('should');
 
 var eml = require('../lib/');
 var AnyfetchClient = require('anyfetch');
+var anyfetchFileHydrater = require('anyfetch-file-hydrater');
 
 process.env.ANYFETCH_API_URL = 'http://localhost:1338';
 
@@ -23,12 +24,7 @@ describe('Test Filecleaner', function() {
       identifier: "azerty",
     };
 
-    var changes = {
-      metadatas: {},
-      user_access: [],
-      actions: {},
-      datas: {}
-    };
+    var changes = anyfetchFileHydrater.defaultChanges();
 
     eml(__dirname + "/samples/osef.lock", document, changes, done);
   });
